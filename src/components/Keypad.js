58c00df13675d03f8e750f680b88f53a929ad186
lisa <lisawanjiku41@gmail.com>
2025-07-12 10:53:03 +0300

    react-hooks-event-handling-lab

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,8 +1,14 @@
 // Code Keypad Component Here
 
+
 function Keypad (){
+    function handleChnage(){
+        console.log("Entering password...")
+    }
     return (
-        <div></div>
+        <div>
+            <input type="password" onChange={handleChnage}/>
+        </div>
     )
 }
 
